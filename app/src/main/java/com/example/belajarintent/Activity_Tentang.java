317a1de08352c9057b0ff9223c1c0ad1afa6b92a
Irsyadagungh08 <irsyadagung08.siswa@smkn4bdg.sch.id>
2020-03-26 21:05:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarintent/Activity_Tentang.java
@@ -0,0 +1,18 @@
+package com.example.belajarintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity_Tentang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__tentang);
+    }
+
+
+}
